8d257550392d8b969d5be426428ae7ec63170f71
komiljonoovd <komiljonooov.d@gmail.com>
2024-12-18 10:12:10 +0500

    Color of Sheets

--- sheets_color.py
+++ sheets_color.py
@@ -5,13 +5,14 @@ wb = openpyxl.load_workbook(filename='input.xlsx', data_only=True)
 
 sheets = wb.active
 
-yellow = "00FF00"
+yellow = "FFFF00"
 
-for i in range(1, sheets.max_row):
+for i in range(1, sheets.max_row+1):
     if i % 2 == 0:
         sheets[f'A{i}'].fill = PatternFill(start_color=yellow, end_color=yellow, fill_type='solid')
         sheets[f'B{i}'].fill = PatternFill(start_color=yellow, end_color=yellow, fill_type='solid')
         sheets[f'C{i}'].fill = PatternFill(start_color=yellow, end_color=yellow, fill_type='solid')
+
 
 wb.save(filename='input.xlsx')
 wb.close()
