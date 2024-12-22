492532930c3fc00d1f0c71312b0fac1afe3d2ee0
komiljonoovd <komiljonooov.d@gmail.com>
2024-12-22 21:43:18 +0500

    Alignment

---
+++ alignment.py
@@ -0,0 +1,16 @@
+import openpyxl
+from openpyxl.styles import Alignment
+
+wb = openpyxl.load_workbook(filename='input.xlsx')
+sheet = wb.active
+
+horizontal = vertical = 'center'
+
+sheet['D2'] = 'Hello'
+sheet['E2'] = 'from'
+sheet['F2'] = 'OpenPyXL'
+
+sheet['D2'].alignment = Alignment(horizontal=horizontal, vertical=vertical)
+sheet['F2'].alignment = Alignment(text_rotation=90)
+
+wb.save(filename='input.xlsx')
